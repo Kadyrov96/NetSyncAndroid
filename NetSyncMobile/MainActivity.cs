1a9b9149e48a9671bc1faef9df2d6a2e2d8775fe
Kadyrov96 <kadyrov96@mail.ru>
2017-03-03 22:10:44 +0300

    Добавьте файлы проекта.

---
+++ NetSyncMobile/MainActivity.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+
+namespace NetSyncMobile
+{
+    [Activity(Label = "NetSyncMobile", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+
+            Button connectSettingsBtn = this.FindViewById<Button>(Resource.Id.connectionMenu);
+            connectSettingsBtn.Click += (sender, e) =>
+            {
+                var intent = new Intent(this, typeof(ConnectionActivity));
+                StartActivity(intent);
+            };
+        }
+    }
+}
+
