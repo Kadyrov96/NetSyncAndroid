35ac3befac734445f0f9423ebfe08f58ec550ffc
Руслан Кадыров < >
2017-05-04 15:26:48 +0300

    Updated project files.

---
+++ NetSync_Mobile/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+
+namespace NetSync_Mobile
+{
+    [Activity(Label = "NetSync_Mobile", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+
+            //Button connectSettingsBtn = FindViewById<Button>(Resource.Layout);
+            //connectSettingsBtn.Click += (sender, e) =>
+            //{
+            //    var intent = new Intent(this, typeof(ConnectionActivity));
+            //    StartActivity(intent);
+            //};
+
+            //Button filePickerBtn = FindViewById<Button>(Resource.Id.folderSelect);
+            //filePickerBtn.Click += (sender, e) =>
+            //{
+            //    var intent = new Intent(this, typeof(FilePickerActivity));
+            //    StartActivity(intent);
+            //};
+        }
+    }
+}
+
--- NetSync_Mobile/Properties/AssemblyInfo.cs
+++ NetSync_Mobile/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NetSyncMobile")]
+[assembly: AssemblyTitle("NetSync_Mobile")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NetSyncMobile")]
+[assembly: AssemblyProduct("NetSync_Mobile")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
