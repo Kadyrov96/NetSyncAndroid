202d99890daa430008c117ecd632aba2371b2764
Руслан Кадыров < >
2017-05-06 22:06:14 +0300

    Added method for showing usual success messages.

--- NetSync_Mobile/MessageDisplayer.cs
+++ NetSync_Mobile/MessageDisplayer.cs
@@ -8,11 +8,30 @@ namespace NetSync_Mobile
         public static void ShowAlertMessage(Activity currentActivity, string title, string message)
         {
             AlertDialog.Builder alert = new AlertDialog.Builder(currentActivity);
-            alert.SetIcon(Resource.Drawable.a);
+            alert.SetIcon(Resource.Drawable.alert);
             alert.SetTitle(title);
             alert.SetMessage(message);
             alert.SetPositiveButton("OK", (senderAlert, args) => {
                 Toast.MakeText(currentActivity, "Accepted!", ToastLength.Short).Show();
+            });
+
+            alert.SetNegativeButton("Cancel", (senderAlert, args) => {
+                Toast.MakeText(currentActivity, "Cancelled!", ToastLength.Short).Show();
+            });
+
+            Dialog dialog = alert.Create();
+            dialog.Show();
+        }
+
+        public static void ShowSuccessMessage(Activity currentActivity, string title, string message)
+        {
+            AlertDialog.Builder alert = new AlertDialog.Builder(currentActivity);
+            alert.SetIcon(Resource.Drawable.Icon);
+            alert.SetTitle(title);
+            alert.SetMessage(message);
+            alert.SetPositiveButton("OK", (senderAlert, args) => {
+                Toast.MakeText(currentActivity, "Accepted!", ToastLength.Short).Show();
+                currentActivity.OnBackPressed();
             });
 
             alert.SetNegativeButton("Cancel", (senderAlert, args) => {
