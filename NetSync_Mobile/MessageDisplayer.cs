6c8175cc71d812288cfae4fdae466e6cb953e138
Руслан Кадыров < >
2017-05-06 14:42:13 +0300

    Class for showing different messages.

---
+++ NetSync_Mobile/MessageDisplayer.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Widget;
+
+namespace NetSync_Mobile
+{
+    class MessageDisplayer
+    {
+        public static void ShowAlertMessage(Activity currentActivity, string title, string message)
+        {
+            AlertDialog.Builder alert = new AlertDialog.Builder(currentActivity);
+            alert.SetIcon(Resource.Drawable.a);
+            alert.SetTitle(title);
+            alert.SetMessage(message);
+            alert.SetPositiveButton("OK", (senderAlert, args) => {
+                Toast.MakeText(currentActivity, "Accepted!", ToastLength.Short).Show();
+            });
+
+            alert.SetNegativeButton("Cancel", (senderAlert, args) => {
+                Toast.MakeText(currentActivity, "Cancelled!", ToastLength.Short).Show();
+            });
+
+            Dialog dialog = alert.Create();
+            dialog.Show();
+        }
+    }
+}
