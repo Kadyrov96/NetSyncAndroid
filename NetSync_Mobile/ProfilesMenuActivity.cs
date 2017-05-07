8a849717f9e0a44f2f64991211b7b7738de3722e
Kadyrov96 <kadyrov96@mail.ru>
2017-05-07 22:55:26 +0300

    Added showing dialog for creating new profile instead of starting new activity.

--- NetSync_Mobile/ProfilesMenuActivity.cs
+++ NetSync_Mobile/ProfilesMenuActivity.cs
@@ -1,5 +1,4 @@
 ﻿using Android.App;
-using Android.Content;
 using Android.Graphics;
 using Android.OS;
 using Android.Views;
@@ -7,6 +6,7 @@ using Android.Widget;
 
 using System.Collections.Generic;
 using System.Linq;
+using Android.Content;
 
 namespace NetSync_Mobile
 {
@@ -14,6 +14,49 @@ namespace NetSync_Mobile
     public class ProfilesMenuActivity : Activity
     {
         ListView profilesList;
+        static LayoutInflater inflater;
+
+        public static void SetPath()
+        {
+            inflater = Application.Context.GetSystemService(LayoutInflaterService) as LayoutInflater;
+            View view = inflater.Inflate(Resource.Layout.AddingProfile, null);
+            EditText profilePath = view.FindViewById<EditText>(Resource.Id.SyncProfilePath);
+            profilePath.Text = "folderpath, bitch";
+        }
+        void CreateAddingDialog()
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            inflater = Application.Context.GetSystemService(LayoutInflaterService) as LayoutInflater;
+            View view = inflater.Inflate(Resource.Layout.AddingProfile, null);
+            builder.SetView(view);
+
+            EditText profileName = view.FindViewById<EditText>(Resource.Id.SyncProfileName);
+            EditText profilePath = view.FindViewById<EditText>(Resource.Id.SyncProfilePath);
+            Button selectFolder_btn = view.FindViewById<Button>(Resource.Id.folderSelectPath);
+
+            selectFolder_btn.Click += (sender, e) =>
+            {
+                var intent = new Intent(this, typeof(FilePickerActivity));
+                StartActivity(intent);
+            };
+
+            builder.SetTitle("Adding new sync profile")
+            .SetPositiveButton("Accept", (senderAlert, args) =>
+            {
+                if (SyncProfilesHandler.AddNewProfile(profileName.Text, profilePath.Text, this))
+                {
+                    MessageDisplayer.ShowSuccessMessage(this, "System notifications", "New profile was successfully added.");
+                }
+                profilesList.Adapter = new CusotmListAdapter(this, SyncProfilesHandler.AvailableProfilesList);
+            })
+            .SetNegativeButton("Cancel", (senderAlert, args) =>
+            {
+                Toast.MakeText(this, "Declined!", ToastLength.Short).Show();
+            });
+
+            Dialog dialog = builder.Create();
+            dialog.Show();
+        }
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -25,11 +68,7 @@ namespace NetSync_Mobile
             profilesList.Adapter = new CusotmListAdapter(this, SyncProfilesHandler.AvailableProfilesList);
 
             Button addProfile_btn = FindViewById<Button>(Resource.Id.addNewProfile);
-            addProfile_btn.Click += (sender, e) =>
-            {
-                var intent = new Intent(this, typeof(AddingProfileActivity));
-                StartActivity(intent);
-            };
+            addProfile_btn.Click += (sender, e) => CreateAddingDialog();
         }
 
         protected override void OnResume()
@@ -37,6 +76,7 @@ namespace NetSync_Mobile
             base.OnResume();
             profilesList.Adapter = new CusotmListAdapter(this, SyncProfilesHandler.AvailableProfilesList);
         }
+
         void OnListItemClick(object sender, AdapterView.ItemClickEventArgs e)
         {
             View selectedItem = profilesList.GetChildAt(e.Position);
@@ -48,13 +88,13 @@ namespace NetSync_Mobile
             {
                 switch (a.Item.ItemId)
                 {
-                    case Resource.Id.pop_button1:   
+                    case Resource.Id.pop_button1:
                         SyncProfilesHandler.DeleteProfile(selectedProfile.ProfileName);
                         profilesList.Adapter = new CusotmListAdapter(this, SyncProfilesHandler.AvailableProfilesList);
                         break;
 
                     case Resource.Id.pop_button2:
-                        if(SyncProfilesHandler.SelectedProfilesList.Any(
+                        if (SyncProfilesHandler.SelectedProfilesList.Any(
                             profile => profile.ProfileName == selectedProfile.ProfileName))
                         {
                             SyncProfilesHandler.SelectedProfilesList.Remove(selectedProfile);
@@ -77,7 +117,7 @@ namespace NetSync_Mobile
         Activity context;
         List<SyncProfile> list;
 
-        public CusotmListAdapter(Activity _context, List<SyncProfile> _list): base()
+        public CusotmListAdapter(Activity _context, List<SyncProfile> _list) : base()
         {
             context = _context;
             list = _list;
@@ -109,8 +149,8 @@ namespace NetSync_Mobile
 
             SyncProfile item = this[position];
             view.FindViewById<TextView>(Resource.Id.ProfileNameRow).Text = item.ProfileName;
-            view.FindViewById<TextView>(Resource.Id.ProfilePathRow).Text = item.ProfileSyncFolderPath;
-            view.FindViewById<TextView>(Resource.Id.SyncDateRow).Text = item.SyncDateTime;
+            view.FindViewById<TextView>(Resource.Id.ProfilePathRow).Text = "Folder path: " + item.ProfileSyncFolderPath;
+            view.FindViewById<TextView>(Resource.Id.SyncDateRow).Text = "Last sync date & time: " + item.SyncDateTime;
 
             return view;
         }
