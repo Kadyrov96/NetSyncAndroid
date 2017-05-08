ff5d3d971de1889fb7212e51908cd2069038e31e
Kadyrov96 <kadyrov96@mail.ru>
2017-05-08 22:53:59 +0300

    Changed and moved last CustomListAdapter class.

---
+++ NetSync_Mobile/Classes/ProfilesListAdapter.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+using Android.App;
+using Android.Views;
+using Android.Widget;
+
+namespace NetSync_Mobile
+{
+    public class ProfilesListAdapter : BaseAdapter<SyncProfile>
+    {
+        Activity context;
+        List<SyncProfile> list;
+
+        public ProfilesListAdapter(Activity _context, List<SyncProfile> _list) : base()
+        {
+            context = _context;
+            list = _list;
+        }
+
+        public override int Count
+        {
+            get { return list.Count; }
+        }
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+        public override SyncProfile this[int index]
+        {
+            get { return list[index]; }
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            View view = convertView;
+
+            // re-use an existing view, if one is available
+            // otherwise create a new one
+            if (view == null)
+                view = context.LayoutInflater.Inflate(Resource.Layout.listitemrow, parent, false);
+
+            SyncProfile item = this[position];
+            view.FindViewById<TextView>(Resource.Id.ProfileNameRow).Text = item.ProfileName;
+            view.FindViewById<TextView>(Resource.Id.ProfilePathRow).Text = "Folder path: " + item.ProfileSyncFolderPath;
+            view.FindViewById<TextView>(Resource.Id.SyncDateRow).Text = "Last sync date & time: " + item.SyncDateTime;
+
+            return view;
+        }
+    }
+}
