005ffd77f11b244ffaa498d285c4e25137d2373b
Kadyrov96 <kadyrov96@mail.ru>
2017-05-08 22:50:21 +0300

    Added static publuc class that holds data which is common for all activites of application.

---
+++ NetSync_Mobile/Classes/AppData.cs
@@ -0,0 +1,9 @@
+﻿namespace NetSync_Mobile
+{
+    public static class AppData
+    {
+        public static string ServerIPAddress { get; set; }
+        public static string ServerHostname { get; set; }
+        public static string SelectedFolderPath { get; set; }
+    }
+}
