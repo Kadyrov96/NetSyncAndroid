2169d86052ae1f513133b03a9f48db9fee2c6cf3
Kadyrov96 <kadyrov96@mail.ru>
2017-05-08 22:47:32 +0300

    Moved class to the "Classes" folder.

---
+++ NetSync_Mobile/Classes/MessageDisplayer.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.Widget;
+
+namespace NetSync_Mobile
+{
+    class MessageDisplayer
+    {
+        static void ShowMessage(Activity currentActivity, string title, string message, int msgTypeFlag)
+        {
+            AlertDialog.Builder alert = new AlertDialog.Builder(currentActivity);
+            alert.SetIcon(msgTypeFlag);
+            alert.SetTitle(title);
+            alert.SetMessage(message);
+            alert.SetPositiveButton("OK", (senderAlert, args) =>
+            {
+                Toast.MakeText(currentActivity, "Accepted!", ToastLength.Short).Show();
+            });
+
+            Dialog dialog = alert.Create();
+            dialog.Show();
+        }
+
+        public static void ShowSuccessMessage(Activity currentActivity, string title, string message)
+        {
+            ShowMessage(currentActivity, title, message, Resource.Drawable.file);
+        }
+
+        public static void ShowAlertMessage(Activity currentActivity, string title, string message)
+        {
+            ShowMessage(currentActivity, title, message, Resource.Drawable.alert);
+        }
+    }
+}
