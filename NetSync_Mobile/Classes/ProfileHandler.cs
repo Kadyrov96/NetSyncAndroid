74ebf54dce17f89f50bbe4080f696b8b73d8ac30
Kadyrov96 <kadyrov96@mail.ru>
2017-05-15 23:01:18 +0300

    Added new class that processes the profile and makes necessary sync operations.

---
+++ NetSync_Mobile/Classes/ProfileHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Security;
+
+namespace NetSync_Mobile
+{
+    class ProfileProcessor
+    {
+        IStreamHandler streamHandler;
+        Synchroniser syncService;
+
+        public ProfileProcessor(SslStream sslStream)
+        {
+            streamHandler = new IOStreamHandler(sslStream);
+        }
+
+        public void ProcessingProfile(SyncProfile profile)
+        {
+            streamHandler.SendString(profile.ProfileName);
+
+            syncService = new Synchroniser(new FolderHandler(profile.ProfileSyncFolderPath));
+            syncService.Synchronise(streamHandler);
+            SyncProfilesHandler.UpdateProfile(profile);
+
+        }
+    }
+}
