02a9df714c19e609b9399fe6019e733f86cc3e16
Kadyrov96 <kadyrov96@mail.ru>
2017-06-11 20:44:37 +0300

    Added material design to the App. Also translated into Russian.

--- NetSync_Mobile/Activities/NetSettingsActivity.cs
+++ NetSync_Mobile/Activities/NetSettingsActivity.cs
@@ -1,16 +1,20 @@
 ﻿using Android.App;
+using Android.Graphics;
+using Android.Graphics.Drawables;
 using Android.OS;
 
 namespace NetSync_Mobile
 {
-    [Activity(Label = "Network settings")]
+    [Activity(Label = "НАСТРОЙКИ ПОДКЛЮЧЕНИЯ")]
     public class NetSettingsActivity : Activity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+            var colorDrawable = new ColorDrawable(Color.DeepSkyBlue);
+            ActionBar.SetBackgroundDrawable(colorDrawable);
+            SetTheme(Android.Resource.Style.ThemeMaterialLight);
             SetContentView(Resource.Layout.ConnectionsSettings);
-            // Create your application here
         }
     }
 }
