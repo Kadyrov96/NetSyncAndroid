d7efb8ee5c04878a9839b00b6590429eeb85f0b7
Kadyrov96 <kadyrov96@mail.ru>
2017-06-12 01:08:26 +0300

    Added fucntionality to resolve conflicts of synchronisation.

---
+++ NetSync_Mobile/Activities/ConflictResolveActivity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Graphics.Drawables;
+using Android.Graphics;
+
+namespace NetSync_Mobile
+{
+    [Activity(Label = "РАЗРЕШЕНИЕ КОНФЛИКТА ФАЙЛОВ")]
+    public class ConflictResolveActivity : Activity
+    {
+        ListView conflictFilesListView;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            AppData.CoflictFilesList = new List<string>();
+            AppData.CoflictFilesList.Add("TestFile3.txt");
+            AppData.CoflictFilesList.Add("TestFile4.txt");
+            AppData.CoflictFilesList.Add("TestFile5.txt");
+
+            var colorDrawable = new ColorDrawable(Color.DeepSkyBlue);
+            ActionBar.SetBackgroundDrawable(colorDrawable);
+
+            SetContentView(Resource.Layout.ConflictLayout);
+            conflictFilesListView = FindViewById<ListView>(Resource.Id.ConflictFilesListView);
+            //profilesListView.ItemClick += OnListItemClick;
+            conflictFilesListView.Adapter = new ConflictFilesListAdapter(this,AppData.CoflictFilesList);
+
+            //ImageButton addProfile_btn = FindViewById<ImageButton>(Resource.Id.addNewProfile_btn);
+        }
+    }
+}
---
+++ NetSync_Mobile/Classes/ConflictFilesListAdapter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Graphics.Drawables;
+using Android.Content.Res;
+
+namespace NetSync_Mobile
+{
+    public class ConflictFilesListAdapter : BaseAdapter<string>
+    {
+        Activity context;
+        List<string> list;
+
+        public ConflictFilesListAdapter(Activity _context, List<string> _list) : base()
+        {
+            context = _context;
+            list = _list;
+        }
+
+        public override int Count
+        {
+            get { return list.Count; }
+        }
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+        public override string this[int index]
+        {
+            get { return list[index]; }
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            View view = convertView;
+
+            // re-use an existing view, if one is available
+            // otherwise create a new one
+            if (view == null)
+                view = context.LayoutInflater.Inflate(Resource.Layout.conflictFileItemRow, parent, false);
+
+            string item = this[position];
+            view.FindViewById<TextView>(Resource.Id.ConflictFileNameRow).Text = item;
+
+            Drawable icon = context.Resources.GetDrawable(Resource.Drawable.alert);
+            view.FindViewById<ImageView>(Resource.Id.ConflictStatusRow).SetImageDrawable(icon);
+
+            return view;
+        }
+    }
+}
